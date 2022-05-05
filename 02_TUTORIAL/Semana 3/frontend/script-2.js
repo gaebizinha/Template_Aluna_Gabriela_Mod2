eab500891bc7b3671fe5e3c29233e12dad082d6c
gaebizinha <99213088+gaebizinha@users.noreply.github.com>
2022-05-05 13:51:05 -0300

    Exercício aula
    Calculadora

---
+++ 02_TUTORIAL/Semana 3/frontend/script-2.js
@@ -0,0 +1,62 @@
+﻿/* 
+=======================
+Declaração de variáveis
+=======================
+*/
+
+
+
+
+/* 
+=======================
+Event Listener
+=======================
+*/
+  
+/* A adição dessa função que "escuta" um evento permite que 
+   verifiquemos se a página foi carregada para só então chamar
+   a função CalcAddAndShow, pois ela só funcionará depois da 
+   criação da div de resultado, com id "result" */
+
+function clicar() {
+    var num1 = document.getElementById("V1").value;
+    var num2 = document.getElementById("V2").value;
+    var resultado = parseInt(num1) + parseInt(num2);
+    document.querySelector(".resultado").innerHTML = resultado;
+}
+/* 
+=======================
+Funções
+======================= 
+*/
+
+/* Função que calcula uma soma e mostra a operação realizada e o resultado obtido */
+function CalcAddAndShow(x, y){
+    var op = "+";
+    var result = Calc(x, y, op);
+    ShowOp(x, y, op);
+    ShowResult(result);
+}
+
+
+/* Função que calcula uma operação entre dois números recebidos como parâmetro */
+function Calc(x, y, op){
+    var result;
+    if (op == "+") {
+        result = x + y;
+    };
+    return result;
+}
+
+
+/* Função que mostra a operação realizada */
+function ShowOp(x, y, op){
+    document.getElementById(calcResDiv).innerHTML += `<br /> ${x} ${op} ${y} = `;
+    //Alternativa: .. += "<br />" + x + " " + op + " " + y + " = "
+}
+
+
+/* Função que mostra o resultado obtido */
+function ShowResult(res){
+    document.getElementById(calcResDiv).innerHTML += res;
+}
