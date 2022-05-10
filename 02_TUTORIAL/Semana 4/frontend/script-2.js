715d1f323d50b9158fe07742dbe0c00fb5748471
gaebizinha <99213088+gaebizinha@users.noreply.github.com>
2022-05-10 20:39:05 -0300

    ETAPA 4

---
+++ 02_TUTORIAL/Semana 4/frontend/script-2.js
@@ -0,0 +1,29 @@
+﻿/* 
+=======================
+Declaração de variáveis
+=======================
+*/
+
+
+
+
+/* 
+=======================
+Event Listener
+=======================
+*/
+  
+/* A adição dessa função que "escuta" um evento permite que 
+   verifiquemos se a página foi carregada para só então chamar
+   a função CalcAddAndShow, pois ela só funcionará depois da 
+   criação da div de resultado, com id "result" */
+
+function clicar() {
+    var num1 = document.getElementById("V1").value;
+    var num2 = document.getElementById("V2").value;
+    var resultado = parseInt(num1) + parseInt(num2);
+    document.querySelector(".resultado").innerHTML = resultado;
+}
+
+
+
