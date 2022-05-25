a08df7f2cdf60ebb3c68cd0594b9ef9de7511ff5
gaebizinha <99213088+gaebizinha@users.noreply.github.com>
2022-05-25 10:13:46 -0300

    ajustes ex 2

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/exer2.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/exer2.js
@@ -1,60 +1,14 @@
-/* 
-=======================
-Declaração de variáveis
-=======================
-*/
+const M1 = 1
+const N2 = 2
+const N5 = 5
+const N10 = 10
+const N20 = 20
+const N50 = 50
+const N100 = 100
+var n1 = document.getElementById("V1").value; 
 
-
-/* 
-=======================
-Event Listener
-=======================
-*/
-  
-/* A adição dessa função que "escuta" um evento permite que 
-   verifiquemos se a página foi carregada para só então chamar
-   a função CalcAddAndShow, pois ela só funcionará depois da 
-   criação da div de resultado, com id "result" */
-
-function clicarSoma() {
-    var n1 = document.getElementById("V1").value;
-    var n2 = document.getElementById("V2").value;
+function clicar() {
+    
     document.querySelector(".resultado1").innerHTML = parseFloat(n1) + parseFloat(n2);
 }
-
-function clicarSub() {
-    var n1 = document.getElementById("V1").value;
-    var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado2").innerHTML = parseFloat(n1) - parseFloat(n2);
-}
-
-function clicarMult() {
-    var n1 = document.getElementById("V1").value;
-    var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado3").innerHTML = parseFloat(n1) * parseFloat(n2);
-}
-
-function clicarPot() {
-    var n1 = document.getElementById("V1").value;
-    var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado4").innerHTML = parseFloat(n1) ** parseFloat(n2);
-}
-
-function clicarDiv() {
-    var n1 = document.getElementById("V1").value;
-    var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado5").innerHTML = parseFloat(n1) / parseFloat(n2);
-}
-
-function clicarRest() {
-    var n1 = document.getElementById("V1").value;
-    var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado6").innerHTML = parseFloat(n1) % parseFloat(n2);
-}
-
-/* 
-=======================
-Funções
-======================= 
-*/
 
--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/script-2.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/script-2.js
@@ -1,2 +1,36 @@
 
+function clicarSoma() {
+    var n1 = document.getElementById("V1").value;
+    var n2 = document.getElementById("V2").value;
+    document.querySelector(".resultado1").innerHTML = parseFloat(n1) + parseFloat(n2);
+}
 
+function clicarSub() {
+    var n1 = document.getElementById("V1").value;
+    var n2 = document.getElementById("V2").value;
+    document.querySelector(".resultado2").innerHTML = parseFloat(n1) - parseFloat(n2);
+}
+
+function clicarMult() {
+    var n1 = document.getElementById("V1").value;
+    var n2 = document.getElementById("V2").value;
+    document.querySelector(".resultado3").innerHTML = parseFloat(n1) * parseFloat(n2);
+}
+
+function clicarPot() {
+    var n1 = document.getElementById("V1").value;
+    var n2 = document.getElementById("V2").value;
+    document.querySelector(".resultado4").innerHTML = parseFloat(n1) ** parseFloat(n2);
+}
+
+function clicarDiv() {
+    var n1 = document.getElementById("V1").value;
+    var n2 = document.getElementById("V2").value;
+    document.querySelector(".resultado5").innerHTML = parseFloat(n1) / parseFloat(n2);
+}
+
+function clicarRest() {
+    var n1 = document.getElementById("V1").value;
+    var n2 = document.getElementById("V2").value;
+    document.querySelector(".resultado6").innerHTML = parseFloat(n1) % parseFloat(n2);
+}
