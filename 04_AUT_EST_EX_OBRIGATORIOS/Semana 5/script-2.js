4e7f4a028ea58025702e6a0727abdd7e75ac8b00
gaebizinha <99213088+gaebizinha@users.noreply.github.com>
2022-05-25 09:46:00 -0300

    exercício de Java etapa 5

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/script-2.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/script-2.js
@@ -19,25 +19,37 @@ Event Listener
 function clicarSoma() {
     var n1 = document.getElementById("V1").value;
     var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado1").innerHTML = parseInt(n1) + parseInt(n2);
+    document.querySelector(".resultado1").innerHTML = parseFloat(n1) + parseFloat(n2);
 }
 
 function clicarSub() {
     var n1 = document.getElementById("V1").value;
     var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado2").innerHTML = parseInt(n1) - parseInt(n2);
+    document.querySelector(".resultado2").innerHTML = parseFloat(n1) - parseFloat(n2);
 }
 
 function clicarMult() {
     var n1 = document.getElementById("V1").value;
     var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado3").innerHTML = parseInt(n1) * parseInt(n2);
+    document.querySelector(".resultado3").innerHTML = parseFloat(n1) * parseFloat(n2);
 }
 
 function clicarPot() {
     var n1 = document.getElementById("V1").value;
     var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado4").innerHTML = parseInt(n1) ** parseInt(n2);
+    document.querySelector(".resultado4").innerHTML = parseFloat(n1) ** parseFloat(n2);
+}
+
+function clicarDiv() {
+    var n1 = document.getElementById("V1").value;
+    var n2 = document.getElementById("V2").value;
+    document.querySelector(".resultado5").innerHTML = parseFloat(n1) / parseFloat(n2);
+}
+
+function clicarRest() {
+    var n1 = document.getElementById("V1").value;
+    var n2 = document.getElementById("V2").value;
+    document.querySelector(".resultado6").innerHTML = parseFloat(n1) % parseFloat(n2);
 }
 
 /* 
