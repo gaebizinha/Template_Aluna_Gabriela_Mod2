7b687664a6e14a02a5aa83a1417489a9cacca925
gaebizinha <99213088+gaebizinha@users.noreply.github.com>
2022-06-02 10:54:00 -0300

    Exercício 1 Etapa 7

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 6/script-2.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 6/script-2.js
@@ -1,36 +1,13 @@
 
 function clicarSoma() {
-    var n1 = document.getElementById("V1").value;
-    var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado1").innerHTML = parseFloat(n1) + parseFloat(n2);
+   var n1 = document.getElementById("V1").value;
+   const n2 = 1
+   document.querySelector(".resultado1").innerHTML = parseInt(n1) + parseInt(n2);
 }
 
 function clicarSub() {
     var n1 = document.getElementById("V1").value;
-    var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado2").innerHTML = parseFloat(n1) - parseFloat(n2);
+    const n2 = 1
+    document.querySelector(".resultado2").innerHTML = parseInt(n1) - parseInt(n2);
 }
 
-function clicarMult() {
-    var n1 = document.getElementById("V1").value;
-    var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado3").innerHTML = parseFloat(n1) * parseFloat(n2);
-}
-
-function clicarPot() {
-    var n1 = document.getElementById("V1").value;
-    var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado4").innerHTML = parseFloat(n1) ** parseFloat(n2);
-}
-
-function clicarDiv() {
-    var n1 = document.getElementById("V1").value;
-    var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado5").innerHTML = parseFloat(n1) / parseFloat(n2);
-}
-
-function clicarRest() {
-    var n1 = document.getElementById("V1").value;
-    var n2 = document.getElementById("V2").value;
-    document.querySelector(".resultado6").innerHTML = parseFloat(n1) % parseFloat(n2);
-}
