d8c6cd6da80a8ab78c3e224e0f8d0c4e54c2749a
gaebizinha <99213088+gaebizinha@users.noreply.github.com>
2022-06-06 13:43:16 -0300

    ex etapa 7

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/script _2.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/script _2.js
@@ -1,5 +1,6 @@
+//função que troca os elementos
 function trocar() {
-   NOME2 = document.getElementById('n2').value
-   document.getElementById('n2').value = document.getElementById('n1').value;
+   NOME2 = document.getElementById('n2').value //var tem o valor atribuido pelo que colocarem no input
+   document.getElementById('n2').value = document.getElementById('n1').value; // varlores colocados em cada input têm valores trocados
    document.getElementById('n1').value = NOME2;
 }
--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/script_1.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/script_1.js
@@ -1,16 +1,17 @@
-var V1 = 0
 
+var V1 = 0
+// função que irá printar na tela o valor indicado na caixa de texto
 function Save() {
    V1 = document.getElementById("V1").value;
    document.querySelector(".resultado1").innerHTML = V1;
    
 }
-
+//soma + 1 ao número da tela  
 function add1() {
    V1 = parseInt(V1) + parseInt(1);
    document.querySelector(".resultado1").innerHTML = V1;
 }
-
+//soma + 1 ao número da tela 
 function less1() {
     V1 = parseInt(V1) - parseInt(1);
     document.querySelector(".resultado1").innerHTML = V1;
