4c15b1b22a74061060af4625606a9a06aa8c5403
gaebizinha <99213088+gaebizinha@users.noreply.github.com>
2022-06-13 10:29:19 -0300

    .

---
+++ 03_AUT_EST_ENTREGA/Semana 8/script.js
@@ -0,0 +1,20 @@
+﻿
+var calcResDiv = "#calc";
+var getResDiv = "#get";
+var getDBResDiv = "#getDB";
+
+/* Função que faz uma requisição GET */
+function TestGETDB(){
+    var url = "http://127.0.0.1:3071/users";
+    var resposta;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, false);
+    xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
+
+    resposta = JSON.parse(xhttp.responseText);
+    
+    $(getDBResDiv).append("<br /><br />" + JSON.stringify(resposta));
+    $(getDBResDiv).append("<br /><br />* Seleção do atributo 'title' do primeiro usuario:<br />" + resposta[0].title);
+    //console.log(xhttp.responseText);
+}
