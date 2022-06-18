04a6fda9572e2a69458c2775e35d15a7070a70f0
gaebizinha <99213088+gaebizinha@users.noreply.github.com>
2022-06-18 16:07:41 -0300

    Atualização ex 4

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/script4.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/script4.js
@@ -1,16 +1,17 @@
+//função que mostra a sequência
 function Seq(){
 var Elem = document.getElementById("V1").value;
 var mostra = document.getElementById("resultado");
-if (Elem <= 0) {
+if (Elem <= 0) {//caso o número colocado na caixa de texto seja negativo ou igual a 0 ira aparecer a mensagem.
    document.querySelector(".resultado").innerHTML = "não é possível calcular";
-}else{
+}else{//caso o número da caixa de texto seja maior que 0, a função dentro do for irá rodar.
    var n1 = 1;
    var n2 = 0;
    var n3 = 0;
    var n4 = 0;
-   var array = [];
+   var array = [];// sequência 
    for(let i = 0; i < Elem; i++){
-      array.push(n3);
+      array.push(n3);//o n3 será o último da sequência
       n3 = n1 + n2;
       n4 = n2/n1;
       n1 = n2;
