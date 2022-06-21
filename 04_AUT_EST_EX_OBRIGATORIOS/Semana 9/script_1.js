dd940e004b97fb04195e4559bb3077a68ebb2ff0
gaebizinha <99213088+gaebizinha@users.noreply.github.com>
2022-06-21 13:56:10 -0300

    .

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/script_1.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/script_1.js
@@ -1,14 +1,10 @@
 //função para calcular se o valor isnerido na caixa de texto é par ou impar 
-function Save() {
+function PI() {
    var V1 = document.getElementById("V1").value;
-   var final = V1%2;
-   if (final == 0) {
-      document.querySelector(".resultado1").innerHTML = "par";
-   } else {
-      document.querySelector(".resultado1").innerHTML = "impar";
-   } 
-   document.querySelector("resultado1").innerHTML = final;
-
+   var array = V1.split("");
+   if (array [0] % 2 == 0) {
+      document.querySelector(".resultado1").innerHTML = "par"
+   }else{
+      document.querySelector(".resultado1").innerHTML = "impar"
+   }
 }
-
-
